b9ad1faeadfb5671cf561bc20a1cd83f22148090
Attituding <84680760+Attituding@users.noreply.github.com>
2022-07-08 23:54:51 -0700

    feat: home feat: persistent nav

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,14 @@
 "use strict";
 const navButton = document.getElementById('navButton');
 const nav = document.getElementById('nav');
+const isHidden = nav.classList.contains('nav');
+const shouldBeHidden = JSON.parse(localStorage.getItem('nav') ?? String(isHidden));
+console.log(`isHidden is ${isHidden}, shouldBeHidden is ${shouldBeHidden}`, localStorage.getItem('nav'));
+if (isHidden !== shouldBeHidden) {
+    nav.classList.toggle('hidden');
+}
 navButton.addEventListener('click', () => {
     nav.classList.toggle('hidden');
+    localStorage.setItem('nav', JSON.stringify(nav.classList.contains('nav')));
+    console.log(`storage is ${localStorage.getItem('nav')}`, nav.classList.contains('hidden'));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 const navButton = document.getElementById('navButton') as HTMLButtonElement;
 const nav = document.getElementById('nav') as HTMLDivElement;
 
+const isHidden = nav.classList.contains('nav');
+
+const shouldBeHidden = JSON.parse(localStorage.getItem('nav') ?? String(isHidden));
+
+console.log(`isHidden is ${isHidden}, shouldBeHidden is ${shouldBeHidden}`, localStorage.getItem('nav'));
+
+if (isHidden !== shouldBeHidden) {
+    nav.classList.toggle('hidden');
+}
+
 navButton.addEventListener('click', () => {
     nav.classList.toggle('hidden');
+    localStorage.setItem('nav', JSON.stringify(nav.classList.contains('nav')));
+    console.log(`storage is ${localStorage.getItem('nav')}`, nav.classList.contains('hidden'));
 });
