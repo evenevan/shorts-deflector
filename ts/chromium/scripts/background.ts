8a535eccdbe2d805d0351f3574b7c1cce6df7726
Attituding <84680760+Attituding@users.noreply.github.com>
2022-07-30 14:09:01 -0700

    refactir: reorder manifest keys refactor: remove async IIFE

--- ts/chromium/scripts/background.ts
+++ ts/chromium/scripts/background.ts
@@ -1,167 +1,128 @@
-(() => {
-    const automaticKey = 'automatic';
+const automaticKey = 'automatic';
 
-    // Legacy Keys
-    const requestKey = 'request';
-    const updateKey = 'update';
+// Legacy Keys
+const requestKey = 'request';
+const updateKey = 'update';
 
-    // Install/Update Handling
-    chrome.runtime.onInstalled.addListener(async (details) => {
-        if (
-            details.reason === chrome.runtime.OnInstalledReason.INSTALL
-            || details.reason === chrome.runtime.OnInstalledReason.UPDATE
-        ) {
-            const keys = await chrome.storage.sync.get(null);
+// Install/Update Handling
+chrome.runtime.onInstalled.addListener(async (details) => {
+    if (
+        details.reason === chrome.runtime.OnInstalledReason.INSTALL
+        || details.reason === chrome.runtime.OnInstalledReason.UPDATE
+    ) {
+        const keys = await chrome.storage.sync.get(null);
 
-            const newKeys = {
-                [automaticKey]:
-                    keys[automaticKey]
-                    ?? (keys[requestKey] || keys[updateKey])
-                    ?? true,
-            };
+        const newKeys = {
+            [automaticKey]:
+                keys[automaticKey]
+                ?? (keys[requestKey] || keys[updateKey])
+                ?? true,
+        };
 
-            await chrome.storage.sync.set(newKeys);
+        await chrome.storage.sync.set(newKeys);
 
-            console.log('Set settings', newKeys);
-        }
-    });
+        console.log('Set settings', newKeys);
+    }
+});
 
-    chrome.webNavigation.onCommitted.addListener(async (details) => {
-        // Listener for new pages with the same URL
+chrome.webNavigation.onCommitted.addListener(async (details) => {
+    // Listener for new pages with the same URL
 
-        if (
-            details.frameId === 0
-            && /^http(s)?:\/\/www\.youtube\.com/.test(details.url)
-            && (
-                details.transitionType === 'auto_bookmark'
-                || details.transitionType === 'link'
-                || details.transitionType === 'reload'
-                || details.transitionType === 'typed'
-            )
-        ) {
-            const tab = await chrome.tabs.get(details.tabId);
-            await handlePageUpdate(details.tabId, tab, 0);
-        }
-    });
+    if (
+        details.frameId === 0
+        && /^http(s)?:\/\/www\.youtube\.com/.test(details.url)
+        && (
+            details.transitionType === 'auto_bookmark'
+            || details.transitionType === 'link'
+            || details.transitionType === 'reload'
+            || details.transitionType === 'typed'
+        )
+    ) {
+        const tab = await chrome.tabs.get(details.tabId);
+        await handlePageUpdate(details.tabId, tab, 0);
+    }
+});
 
-    chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
-        // Listener for new pages with new URLs
+chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
+    // Listener for new pages with new URLs
 
-        if (
-            details.frameId === 0
-            && /^http(s)?:\/\/www\.youtube\.com/.test(details.url)
-        ) {
-            const tab = await chrome.tabs.get(details.tabId);
-            await handlePageUpdate(details.tabId, tab, 1);
-        }
-    });
+    if (
+        details.frameId === 0
+        && /^http(s)?:\/\/www\.youtube\.com/.test(details.url)
+    ) {
+        const tab = await chrome.tabs.get(details.tabId);
+        await handlePageUpdate(details.tabId, tab, 1);
+    }
+});
 
-    let latestRequest: string | null = null;
+let latestRequest: string | null = null;
 
-    async function handlePageUpdate(tabId: number, tab: chrome.tabs.Tab, source: number) {
-        // Request Dedupe
-        if (latestRequest === `${tab.id}${tab.url}` && source !== 0) {
-            return;
-        }
+async function handlePageUpdate(tabId: number, tab: chrome.tabs.Tab, source: number) {
+    // Request Dedupe
+    if (latestRequest === `${tab.id}${tab.url}` && source !== 0) {
+        return;
+    }
 
-        const regex = /^http(s)?:\/\/www\.youtube\.com\/shorts\/(.+)$/;
-        const url = tab.url?.match(regex);
+    const regex = /^http(s)?:\/\/www\.youtube\.com\/shorts\/(.+)$/;
+    const url = tab.url?.match(regex);
 
-        const { [automaticKey]: automatic } = await chrome.storage.sync.get([
-            automaticKey,
-        ]);
+    const { [automaticKey]: automatic } = await chrome.storage.sync.get([
+        automaticKey,
+    ]);
 
-        if (automatic === false) {
-            return;
-        }
+    if (automatic === false) {
+        return;
+    }
 
-        if (url) {
-            // Redirecting
+    if (url) {
+        // Redirecting
 
-            latestRequest = `${tab.id}${tab.url}`;
+        latestRequest = `${tab.id}${tab.url}`;
 
-            const cleanURL = url[0].replace('shorts/', 'watch?v=');
+        const cleanURL = url[0].replace('shorts/', 'watch?v=');
 
-            await chrome.tabs.update(tabId, {
-                url: cleanURL,
-            });
-        } else {
-            // URL Updating
+        await chrome.tabs.update(tabId, {
+            url: cleanURL,
+        });
+    } else {
+        // URL Updating
 
-            latestRequest = `${tab.id}${tab.url}`;
+        latestRequest = `${tab.id}${tab.url}`;
 
-            await chrome.scripting.executeScript({
-                target: {
-                    tabId: tabId,
-                },
-                func: replaceURLsExecute,
-            });
+        await chrome.scripting.executeScript({
+            target: {
+                tabId: tabId,
+            },
+            func: replaceURLsExecute,
+        });
+    }
+}
+
+function replaceURLsExecute() {
+    // @ts-ignore different scope at runtime
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    const regex = /^http(s)?:\/\/www\.youtube\.com\/shorts\/(.+)$/;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const anchor of document.getElementsByTagName('a')) {
+        if (anchor.href.match(regex)) {
+            anchor.href = anchor.href.replace('shorts/', 'watch?v=');
+
+            anchor.addEventListener('click', (event) => {
+                event.stopImmediatePropagation();
+            }, true);
         }
     }
 
-    function replaceURLsExecute() {
-        // @ts-ignore different scope at runtime
-        // eslint-disable-next-line @typescript-eslint/no-shadow
-        const regex = /^http(s)?:\/\/www\.youtube\.com\/shorts\/(.+)$/;
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const anchor of document.getElementsByTagName('a')) {
-            if (anchor.href.match(regex)) {
-                anchor.href = anchor.href.replace('shorts/', 'watch?v=');
-
-                anchor.addEventListener('click', (event) => {
-                    event.stopImmediatePropagation();
-                }, true);
-            }
-        }
-
-        const observer = new MutationObserver((mutationRecords) => {
-            // Only works on YouTube, fast & reliable
-            mutationRecords.forEach((mutationRecord) => {
-                if (
-                    mutationRecord.type === 'attributes'
-                    && mutationRecord.target.nodeType === 1
-                    && mutationRecord.target.nodeName === 'A'
-                ) {
-                    const anchor = mutationRecord.target as HTMLAnchorElement;
-                    if (anchor.href.match(regex)) {
-                        anchor.href = anchor.href.replace('shorts/', 'watch?v=');
-
-                        anchor.addEventListener('click', (event) => {
-                            event.stopImmediatePropagation();
-                        }, true);
-                    }
-                }
-            });
-
-            /*
-            // Works on all tested sites, slightly buggy at times
-            const uniqueClass = 'shorts-deflector';
-
-            const addedNodes = mutationRecords.map(
-                (record) => Object.values(record.addedNodes),
-            ).flat(1);
-
-            const addedElements = addedNodes.filter(
-                (addedNode) => addedNode.nodeType === 1
-                && addedNode.childNodes.length !== 0,
-            ) as HTMLElement[];
-
-            const anchors = addedElements.map(
-                (addedElement) => Object.values(
-                    addedElement.getElementsByTagName('a'),
-                ),
-            ).flat(1);
-
-            anchors.forEach((anchor) => {
-                const isNotNew = anchor.classList.contains(uniqueClass);
-
-                if (isNotNew) {
-                    return;
-                }
-
-                anchor.classList.add(uniqueClass);
-
+    const observer = new MutationObserver((mutationRecords) => {
+        // Only works on YouTube, fast & reliable
+        mutationRecords.forEach((mutationRecord) => {
+            if (
+                mutationRecord.type === 'attributes'
+                && mutationRecord.target.nodeType === 1
+                && mutationRecord.target.nodeName === 'A'
+            ) {
+                const anchor = mutationRecord.target as HTMLAnchorElement;
                 if (anchor.href.match(regex)) {
                     anchor.href = anchor.href.replace('shorts/', 'watch?v=');
 
@@ -169,15 +130,52 @@
                         event.stopImmediatePropagation();
                     }, true);
                 }
-            });
-            */
+            }
         });
 
-        observer.observe(document.body, {
-            attributes: true,
-            attributeFilter: ['href'],
-            childList: true,
-            subtree: true,
+        /*
+        // Works on all tested sites, slightly buggy at times
+        const uniqueClass = 'shorts-deflector';
+
+        const addedNodes = mutationRecords.map(
+            (record) => Object.values(record.addedNodes),
+        ).flat(1);
+
+        const addedElements = addedNodes.filter(
+            (addedNode) => addedNode.nodeType === 1
+            && addedNode.childNodes.length !== 0,
+        ) as HTMLElement[];
+
+        const anchors = addedElements.map(
+            (addedElement) => Object.values(
+                addedElement.getElementsByTagName('a'),
+            ),
+        ).flat(1);
+
+        anchors.forEach((anchor) => {
+            const isNotNew = anchor.classList.contains(uniqueClass);
+
+            if (isNotNew) {
+                return;
+            }
+
+            anchor.classList.add(uniqueClass);
+
+            if (anchor.href.match(regex)) {
+                anchor.href = anchor.href.replace('shorts/', 'watch?v=');
+
+                anchor.addEventListener('click', (event) => {
+                    event.stopImmediatePropagation();
+                }, true);
+            }
         });
-    }
-})();
+        */
+    });
+
+    observer.observe(document.body, {
+        attributes: true,
+        attributeFilter: ['href'],
+        childList: true,
+        subtree: true,
+    });
+}
