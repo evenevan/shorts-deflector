ea40ec44bc85c28f09306713daff4363c8e88b75
Attituding <84680760+Attituding@users.noreply.github.com>
2022-08-30 14:31:11 -0700

    refactor: tailwind config fix: privacy policy wording

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({})],
+    integrations: [mdx(), tailwind({ config: {
+        applyBaseStyles: false,
+    }})],
     site: 'https://shortsdeflector.attituding.live',
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        './src/styles/base.css'
+    ],
 	theme: {
-		extend: {},
+		extend: {
+            colors: {
+                'youtube': '#f40407',
+            },
+        },
 	},
 	plugins: [],
 }
