a13ac8394e4d7abdb32619f42433002fbf940839
Attituding <84680760+Attituding@users.noreply.github.com>
2022-08-30 15:22:58 -0700

    refactor: single source of truth for color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,26 @@ module.exports = {
             colors: {
                 'youtube': '#f40407',
             },
+            backgroundColor: {
+                'dark-1': '#171717',
+                'dark-2': '#262626',
+                'dark-3': '#404040',
+                'light-1': '#ffffff',
+                'light-2': '#f5f5f5',
+                'light-3': '#e5e5e5',
+            },
+            borderColor: {
+                'dark-1': '#404040',
+                'light-1': '#e5e5e5',
+            },
+            textColor: {
+                'dark-1': '#ffffff',
+                'dark-2': '#d4d4d4',
+                'dark-3': '#a3a3a3',
+                'light-1': '#000000',
+                'light-2': '#404040',
+                'light-3': '#525252',
+            },
         },
 	},
 	plugins: [],
