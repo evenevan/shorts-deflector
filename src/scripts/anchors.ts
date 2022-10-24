2ffb1b89cd0c87bd9c065fb2512a756defdf2fc5
Attituding <84680760+Attituding@users.noreply.github.com>
2022-10-23 19:32:52 -0700

    fix: empty getElementById

--- src/scripts/anchors.ts
+++ src/scripts/anchors.ts
@@ -1,7 +1,7 @@
 export {};
 
 const cleanHash = window.location.hash.slice(1);
-const selectedHash = document.getElementById(cleanHash) as HTMLAnchorElement;
+const selectedHash = cleanHash && document.getElementById(cleanHash) as HTMLAnchorElement;
 
 if (selectedHash) {
     const top = selectedHash.getBoundingClientRect().top + window.pageYOffset;
